adedebc6f9bffa5914e1aa8902672f1ca611ff38
Serhii Hamotskyi <serhii.hamotskyi@hs-anhalt.de>
2025-08-06 16:12:03 +0200

    #0 Checkpoint

--- configs/unsymbols/ours_hier_ord.py
+++ configs/unsymbols/ours_hier_ord.py
@@ -20,16 +20,28 @@ class Config(Config):
         self.filter_category = None
 
         self.learning_rate = 1e-3 * num_gpus
-        self.batch_size = 60 * num_gpus
+        # self.batch_size = 60 * num_gpus
+        self.batch_size = 20
+
+        # default 500
+        self.warmup_steps = 1000  #
 
         # our changes
         self.val_every = 100
         self.ckpt_every = 100
 
-        # 50 default, we can --resume training 
+        # 50 default, we can --resume training
         self.num_epochs = 400
 
-        self.dataloader_module = "deepsvg.svg_dataset"  #
-        self.collate_fn = None                                #
-        self.data_dir = "/home/sh/o/unsymbols/DATASETS/deepsvg/full_svg_simplified"             #
-        self.meta_filepath = "/home/sh/o/unsymbols/DATASETS/deepsvg/meta_full_svg.csv"       #
+        self.dataloader_module = "deepsvg.svg_dataset"
+        self.collate_fn = None  #
+        # self.data_dir = "/home/sh/o/unsymbols/DATASETS/deepsvg/full_svg_simplified"             #
+        self.data_dir = (
+            "/home/sh/o/unsymbols/preprocessing/data/22-0.7-svg-preprocessed/"  #
+        )
+        # self.data_dir = "/home/sh/o/unsymbols/preprocessing/data/0.5-svg/"  #
+        # self.meta_filepath = "/home/sh/o/unsymbols/DATASETS/deepsvg/meta_full_svg.csv"       #
+        self.meta_filepath = (
+            "/home/sh/o/unsymbols/preprocessing/data/22-0.7-svg-preprocessed/meta.csv"
+        )
+        # self.meta_filepath = "/tmp/meta.csv"
